80976f2b801a8589e2dd30cb16321ec8c13b626c
cris <cristian.ricardo.elias@gmail.com>
2022-10-12 00:14:06 -0300

    Conditional example

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import "./App.css";
 import { css } from "@emotion/react";
 
 function App() {
+  const [bool, setBool] = useState(false);
+
+  const myStyle = css`
+    border: 1px solid hotpink;
+    border-radius: 8px;
+    padding: 25px 50px;
+    font-size: ${bool ? "28px" : "80px"};
+    color: black;
+    cursor: pointer;
+    user-select: none;
+  `;
+
   return (
-    <div
-      css={css`
-        font-size: 28px;
-        color: white;
-      `}
-    >
-      {" "}
-      Ejemplo estilos emotion
+    <div onClick={() => setBool(!bool)}>
+      <h1 css={myStyle}>{bool ? "toy shikitoh" : "anma, ya creci"}</h1>
     </div>
   );
 }
